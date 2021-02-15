9504a5e800cd9a553797cb54a10c6add5c5776d4
Amanda Nikrant <ajnikrant@gmail.com>
2021-02-15 16:17:10 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -30,7 +30,7 @@ def apply_clearance(cart)
   counter =0
   cart_item = find_item_by_name_in_collection(coupons[counter][:item], cart)
   while counter < cart.length
-    if cart_item 
+    if cart_item
       if cart[counter][:clearance] == true
         clearanced_item = {
           :item => cart[counter][:item],
