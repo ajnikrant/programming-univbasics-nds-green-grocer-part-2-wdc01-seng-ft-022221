91b472ddf0bd3d2ea63eb8a945a49d96528a5947
Amanda Nikrant <ajnikrant@gmail.com>
2021-02-15 16:31:10 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -49,6 +49,8 @@ def apply_clearance(cart)
 end
 
 def checkout(cart, coupons)
+  binding.pry
+
   counter = 0
   consolidated_cart= consolidate_cart(cart)
   couponed_cart = apply_coupons(consolidate_cart)
