6bdf7ff8dcaa9013e6d39ee07a98aaf6af8e5932
Amanda Nikrant <ajnikrant@gmail.com>
2021-02-15 16:38:17 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -51,11 +51,11 @@ end
 def checkout(cart, coupons)
   consolidated_cart = consolidate_cart(cart)
   couponed_cart = apply_coupons(consolidated_cart, coupons)
-  clearanced_cart = apply_clearance(couponed_cart)
+  final_cart = apply_clearance(couponed_cart)
   counter = 0
-  grand_total = []
+  grand_total = 0
 
-  while counter < clearanced_cart
+  while counter < final_cart
     grand_total << clearanced_cart[counter][:price]
     counter +=1
   end
